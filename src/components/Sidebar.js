3b71024518b6117b2b7a8749443fb4088d81fc1e
dotslashf <fadhlurahman.i@gmail.com>
2021-10-22 21:29:02 +0800

    Add dropdown content sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,17 @@
 import SidebarItem from './sidebar/SidebarItem';
-import { HiEmojiHappy, HiHand, HiLightningBolt, HiPlus } from 'react-icons/hi';
+import { HiHand, HiLightningBolt, HiPlus } from 'react-icons/hi';
+import { FaDiscord } from 'react-icons/fa';
 
 function Sidebar() {
   return (
-    <div className="relative flex flex-col top-0 left-0 items-center w-[4.5rem] bg-discord-shade-darkest text-white h-screen m-0 py-3">
+    <div className="relative flex flex-col top-0 left-0 items-center w-[4.5rem] bg-discord-shade-darker text-white h-screen m-0 py-2">
       <SidebarItem
-        icon={<HiEmojiHappy size="28" />}
+        icon={<FaDiscord size="28" />}
         isPillShown={true}
         notifCount={69}
         tooltipText="Home"
       />
-      <div className="border opacity-60 border-gray-600 px-4 mt-1 mb-1" />
+      <div className="border opacity-40 border-gray-600 px-3 mt-1 mb-1" />
       <SidebarItem icon={<HiPlus size="28" />} tooltipText="Add Server" />
       <SidebarItem
         icon={<HiHand size="28" />}
--- src/components/content.js/Dropdown.js
+++ src/components/content.js/Dropdown.js
@@ -1,7 +1,16 @@
 import { Menu, Transition } from '@headlessui/react';
 import { Fragment } from 'react';
-import { HiChevronDown, HiUserAdd } from 'react-icons/hi';
-import { BsSuitDiamondFill } from 'react-icons/bs';
+import {
+  HiChevronDown,
+  HiX,
+  HiUserAdd,
+  HiCheck,
+  HiBell,
+  HiShieldCheck,
+  HiLogin,
+  HiPencil,
+} from 'react-icons/hi';
+import { BsSuitDiamondFill, BsCheckSquareFill } from 'react-icons/bs';
 
 export default function Dropdown({ serverName }) {
   return (
@@ -10,70 +19,208 @@ export default function Dropdown({ serverName }) {
         as="div"
         className="flex text-left relative items-center justify-center"
       >
-        <div className="relative w-full px-4 py-4 border-b-2 border-discord-shade-darkest shadow-md">
-          <Menu.Button className="inline-flex w-full justify-between text-sm font-semibold text-discord-shade-grayLight">
-            <div>{serverName}</div>
-            <div>
-              <HiChevronDown
-                className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
-                aria-hidden="true"
-              />
-            </div>
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute w-11/12 top-16 origin-top bg-discord-shade-darkest divide-y rounded-sm shadow-md">
-            <div className="px-2 py-3 text-xs">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active
-                        ? 'bg-discord-blue-default text-white'
-                        : 'text-discord-shade-gray'
-                    } group flex justify-between rounded-sm items-center w-full px-2 py-2 text-sm`}
-                  >
-                    Server Boost
-                    {active ? (
-                      <BsSuitDiamondFill size="16" className="text-white" />
-                    ) : (
-                      <BsSuitDiamondFill size="16" className="text-pink-400" />
-                    )}
-                  </button>
+        {({ open }) => (
+          <>
+            <div className="relative w-full px-4 py-3 border-b-2 border-discord-shade-darker shadow-md">
+              <Menu.Button className="inline-flex w-full justify-between font-medium items-center text-discord-shade-grayLight">
+                <div>{serverName}</div>
+                {open ? (
+                  <div>
+                    <HiX className="w-5 h-5 ml-2 -mr-1" aria-hidden="true" />
+                  </div>
+                ) : (
+                  <div>
+                    <HiChevronDown
+                      className="w-5 h-5 ml-2 -mr-1"
+                      aria-hidden="true"
+                    />
+                  </div>
                 )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active
-                        ? 'bg-discord-blue-default text-white'
-                        : 'text-discord-shade-gray'
-                    } group flex justify-between rounded-sm items-center w-full px-2 py-2 text-sm`}
-                  >
-                    Invite People
-                    {active ? (
-                      <HiUserAdd size="16" className="text-white" />
-                    ) : (
-                      <HiUserAdd
-                        size="16"
-                        className="text-discord-blue-light"
-                      />
-                    )}
-                  </button>
-                )}
-              </Menu.Item>
+              </Menu.Button>
             </div>
-          </Menu.Items>
-        </Transition>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute w-11/12 top-14 origin-top bg-discord-shade-darkest divide-y rounded-md shadow-md">
+                <div className="px-2 py-1.5">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active
+                            ? 'bg-discord-blue-default text-white'
+                            : 'text-discord-shade-gray'
+                        } group flex justify-between rounded-sm items-center w-full px-2 py-1.5 text-sm`}
+                      >
+                        Server Boost
+                        {active ? (
+                          <BsSuitDiamondFill size="16" className="text-white" />
+                        ) : (
+                          <BsSuitDiamondFill
+                            size="16"
+                            className="text-pink-400"
+                          />
+                        )}
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <hr className="border-t my-1 border-discord-shade-grayLight border-opacity-10" />
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active
+                            ? 'bg-discord-blue-default text-white'
+                            : 'text-discord-blue-light'
+                        } group flex justify-between rounded-sm items-center w-full px-2 py-1.5 text-sm`}
+                      >
+                        Invite People
+                        {active ? (
+                          <HiUserAdd size="16" className="text-white" />
+                        ) : (
+                          <HiUserAdd
+                            size="16"
+                            className="text-discord-blue-light"
+                          />
+                        )}
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <hr className="border-t my-1 border-discord-shade-grayLight border-opacity-10" />
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active
+                            ? 'bg-discord-blue-default text-white'
+                            : 'text-discord-shade-gray'
+                        } group flex justify-between rounded-sm items-center w-full px-2 py-1.5 text-sm`}
+                      >
+                        Mark As Read
+                        {active ? (
+                          <HiCheck size="16" className="text-white" />
+                        ) : (
+                          <HiCheck
+                            size="16"
+                            className="text-discord-shade-gray"
+                          />
+                        )}
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active
+                            ? 'bg-discord-blue-default text-white'
+                            : 'text-discord-shade-gray'
+                        } group flex justify-between rounded-sm items-center w-full px-2 py-1.5 text-sm`}
+                      >
+                        Notification Settings
+                        {active ? (
+                          <HiBell size="16" className="text-white" />
+                        ) : (
+                          <HiBell
+                            size="16"
+                            className="text-discord-shade-gray"
+                          />
+                        )}
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active
+                            ? 'bg-discord-blue-default text-white'
+                            : 'text-discord-shade-gray'
+                        } group flex justify-between rounded-sm items-center w-full px-2 py-1.5 text-sm`}
+                      >
+                        Privacy Settings
+                        {active ? (
+                          <HiShieldCheck size="16" className="text-white" />
+                        ) : (
+                          <HiShieldCheck
+                            size="16"
+                            className="text-discord-shade-gray"
+                          />
+                        )}
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <hr className="border-t my-1 border-discord-shade-grayLight border-opacity-10" />
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active
+                            ? 'bg-discord-blue-default text-white'
+                            : 'text-discord-shade-gray'
+                        } group flex justify-between rounded-sm items-center w-full px-2 py-1.5 text-sm`}
+                      >
+                        Edit Server Profile
+                        {active ? (
+                          <HiPencil size="16" className="text-white" />
+                        ) : (
+                          <HiPencil
+                            size="16"
+                            className="text-discord-shade-gray"
+                          />
+                        )}
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active
+                            ? 'bg-discord-blue-default text-white'
+                            : 'text-discord-shade-gray'
+                        } group flex justify-between rounded-sm items-center w-full px-2 py-1.5 text-sm`}
+                      >
+                        Hide Muted Channels
+                        {active ? (
+                          <BsCheckSquareFill size="14" className="text-white" />
+                        ) : (
+                          <BsCheckSquareFill
+                            size="14"
+                            className="text-discord-shade-gray"
+                          />
+                        )}
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <hr className="border-t my-1 border-discord-shade-grayLight border-opacity-10" />
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? 'bg-red-600 text-white' : 'text-red-600'
+                        } group flex justify-between rounded-sm items-center w-full px-2 py-1.5 text-sm`}
+                      >
+                        Leave Server
+                        {active ? (
+                          <HiLogin size="16" className="text-white" />
+                        ) : (
+                          <HiLogin size="16" className="text-red-600" />
+                        )}
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </>
+        )}
       </Menu>
     </>
   );
